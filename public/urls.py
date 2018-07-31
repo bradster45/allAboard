8442fc9974f1d5348de4770fcdeb2816da0ca98c
Bradley Fordham <bradleyfordham@Bradleys-MacBook.local>
2018-08-01 00:45:18 +0100

    need to debug csrf token

--- public/urls.py
+++ public/urls.py
@@ -5,7 +5,7 @@ from public.views import *
 urlpatterns = [
     # class based
     url(r'^$', HomeTemplateView.as_view(), name='home'),
-    # url(r'^workout-create/$', WorkoutCreateView.as_view(), name='create_workout'),
+    url(r'^workout-create/$', WorkoutCreateView.as_view(), name='create_workout'),
     # api
     url(r'^api/workout-setup/$', setupWorkout, name='setup_workout'),
 ]
